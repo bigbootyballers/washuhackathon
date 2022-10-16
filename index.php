75779a4f3f99e7a4a090f170a0e293514856d87f
Fisher Sun <fisher521.fs@gmail.com>
2022-10-16 04:37:44 -0500

    Factor out create an event

--- index.php
+++ index.php
@@ -33,42 +33,8 @@ include "includes/head.php";
     <h2>
         Create an event
     </h2>
-    <form method="POST">
-        <div>
-            <label for="name">Event name: </label>
-            <input type="text" id="name" name="name" required>
-        </div>
+    <a href="event_create.php">Create an event</a>
 
-        <div>
-            <label for="date">Date: </label>
-            <input type="date" id="date" name="date"
-                   value="<?php echo date("Y-m-d");?>" required>
-        </div>
-
-        <fieldset>
-            <legend>Private/public:</legend>
-            <div>
-                <input type="radio" name="private/public" id="private" value="private" checked required>
-                <label for="private">Private</label>
-            </div>
-
-            <div>
-                <input type="radio" name="private/public" id="public" value="public" required>
-                <label for="public">Public</label>
-            </div>
-        </fieldset>
-
-        <div>
-            <input type="submit" value="Submit">
-        </div>
-    </form>
-    <?php
-    if (isset($_POST["name"])) {
-        create_event($_POST["name"], $_POST["date"], $_POST["private/public"] == "private");
-        join_last_event($_SESSION["username"]);
-        printf("<p>Created and joined event \"%s\"!</p>", $_POST["name"]);
-    }
-    ?>
     <h2>
         Upcoming events
     </h2>
