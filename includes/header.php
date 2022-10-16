175b9ea3cfa7f54d8450b5970628b7508cb8f667
dspet <dspet@DESKTOP-RN8PU0N>
2022-10-16 10:08:50 -0500

    remove headers

--- includes/header.php
+++ includes/header.php
@@ -6,8 +6,6 @@
     <nav>
         <ul>
             <li><a href="index.php">Home</a></li>
-            <li><a href="event.php">Map</a></li>
-            <li><a href="group_join.php">Join a Group</a></li>
         </ul>
     </nav>
 </header>
