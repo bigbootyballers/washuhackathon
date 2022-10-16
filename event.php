a8a7ad9299d57fe0b55e030fa6fe541f59dc967d
dspet <dspet@DESKTOP-RN8PU0N>
2022-10-16 09:40:55 -0500

    .

--- event.php
+++ event.php
@@ -65,24 +65,7 @@ include "includes/head.php";
         <script>
         function getJSON() {
             const myJSONString = JSON.stringify(polyPoints);
-            alert($('#csv').text(ConvertToCSV(myJSONString)));
-        }
-        function ConvertToCSV(objArray) {
-            var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
-            var str = '';
-
-            for (var i = 0; i < array.length; i++) {
-                var line = '';
-                for (var index in array[i]) {
-                    if (line != '') line += ','
-
-                    line += array[i][index];
-                }
-
-                str += line + '\r\n';
-            }
-
-            return str;
+            alert(myJSONString);
         }
         </script>
 
