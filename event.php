561ed3901e6bf9a60c6ab160afe01ec30d7f4bfc
dspet <dspet@DESKTOP-RN8PU0N>
2022-10-16 09:57:25 -0500

    .

--- event.php
+++ event.php
@@ -66,15 +66,8 @@ include "includes/head.php";
         function getJSON() {
             const myJSONString = JSON.stringify(polyPoints);
             alert(myJSONString);
-            download(myJSONString, "Points.txt", "text/plain");
         }
-        function download(content, fileName, contentType) {
-            const a = document.createElement("a");
-            const file = new Blob([content], { type: contentType });
-            a.href = URL.createObjectURL(file);
-            a.download = fileName;
-            a.click();
-        }
+
 }
         </script>
 
